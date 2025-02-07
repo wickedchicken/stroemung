c0b1886b9898d33676db1b261acd769efb5084ee
Mike Stipicevic <stipredirect@gmail.com>
2025-02-07 16:01:59 +0100

    Add ability to parse NaSt2D output files

--- python/test_generate_test_data.py
+++ python/test_generate_test_data.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
+
+
 import generate_test_data
 
+import json
 
-def test_capital_case():
-    assert generate_test_data.main(None) == 0
+
+TEST_DATA_DIR = Path(Path(__file__).parent.resolve(), 'test_data')
+
+
+def load_expected_simulation(filename):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+
+    return generate_test_data.SimulationOutput(**data)
+
+
+def test_data_parsing():
+    filename = 'small_data.out'
+
+    with open(Path(TEST_DATA_DIR, filename), 'rb') as f:
+        parsed_output = generate_test_data.parse_out_file(f)
+
+    expected = load_expected_simulation(
+        Path(TEST_DATA_DIR, filename + '_expected.json')
+    )
+
+    assert parsed_output == expected
