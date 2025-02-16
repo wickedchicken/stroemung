130c7ab353ca7bf7f0c5f19e0376e991d06c3e02
Mike Stipicevic <stipredirect@gmail.com>
2025-02-16 15:53:02 +0100

    Serialize using serde_json

--- src/grid.rs
+++ src/grid.rs
@@ -1,11 +1,13 @@
 use std::fmt;
 
 use ndarray::Array;
+use serde::Deserialize;
+use serde::Serialize;
 
 use crate::math::Real;
 use crate::types::{GridArray, GridSize};
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct SimulationGrid {
     size: GridSize,
     pressure: GridArray<Real>,
@@ -39,5 +41,16 @@ mod tests {
         assert_eq!(grid.size[0], 5);
         assert_eq!(grid.size[1], 7);
         assert_eq!(grid.pressure.shape(), size);
+    }
+
+    #[test]
+    fn serialize() {
+        let size = [2, 3];
+        let grid = SimulationGrid::new(size);
+        let serialized = serde_json::to_string(&grid).unwrap();
+        assert_eq!(
+            serialized,
+            "{\"size\":[2,3],\"pressure\":{\"v\":1,\"dim\":[2,3],\"data\":[0.0,0.0,0.0,0.0,0.0,0.0]}}"
+        );
     }
 }
