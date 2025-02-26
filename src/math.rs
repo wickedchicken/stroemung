a5098d247693c0b2d51ebb0e23ea4701cc854464
Mike Stipicevic <stipredirect@gmail.com>
2025-02-26 21:58:45 +0100

    Convert du2dx_kernel to use ArrayView2
    This will make it easier to use when combined with
    other functions in the future.

--- src/math.rs
+++ src/math.rs
@@ -1,4 +1,4 @@
-use ndarray::ArrayView1;
+use ndarray::ArrayView2;
 
 pub type Real = f64;
 
@@ -10,15 +10,16 @@ pub type Real = f64;
 ///
 /// # Arguments
 ///
-/// * `u_view` - A 3-element ArrayView1 representing u[(i-1), j],  u[(i, j)]
-///   and u[(i+1, j)]. Since all three elements are on the same axis, the j
-///   index is unnecessary and the i index is relative: [-1, 0, 1].
+/// * `u_view` - A 3x3-element ArrayView2 representing
+///   u[(i-1) to (i+1), (j-1) to (j+1)]. This function only uses
+///   the three values on the column where j is 0 (index 1), but takes a 3x3
+///   ArrayView2 to be easier to combine with other functions.
 /// * `gamma` - Greek letter gamma, the upwind discretization parameter
 /// * `delx` - "delta x," the physical width of the cell
-pub fn du2dx_kernel(u_view: ArrayView1<Real>, delx: Real, gamma: Real) -> Real {
-    let u_i_m1 = u_view[0]; // u[(i-1, j)]  "u[i minus 1]" -> u_i_m1
-    let u_i = u_view[1]; // u[(i, j)]  "u[i]" -> "u_i"
-    let u_i_p1 = u_view[2]; // u[(i+1, j)]  "u[i plus 1]" -> u_i_p1
+pub fn du2dx_kernel(u_view: ArrayView2<Real>, delx: Real, gamma: Real) -> Real {
+    let u_i_m1 = u_view[(0, 1)]; // u[(i-1, j)]  "u[i minus 1]" -> u_i_m1
+    let u_i = u_view[(1, 1)]; // u[(i, j)]  "u[i]" -> "u_i"
+    let u_i_p1 = u_view[(2, 1)]; // u[(i+1, j)]  "u[i plus 1]" -> u_i_p1
 
     let inner_left1 = (u_i + u_i_p1).powi(2);
     let inner_right1 = (u_i_m1 + u_i).powi(2);
@@ -34,21 +35,51 @@ pub fn du2dx_kernel(u_view: ArrayView1<Real>, delx: Real, gamma: Real) -> Real {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use ndarray::{array, ArrayView1};
+    use ndarray::{array, ArrayView2};
 
     #[test]
     fn test_du2dx_kernel() {
         // These don't have any particular significance, just some random data.
         let test_cases = [
-            (array![1., 2., 3.], 1., 1.7, 3.15),
-            (array![-1., 2., 3.], 1., 1.7, 5.15),
-            (array![1., 1., 1.], 1., 1.7, 0.),
-            (array![1., 2., 3.], 1., 1.3, 3.35),
-            (array![1., 2., 3.], 1.5, 1.7, 2.1),
-            (array![10., 20., 30.], 1.5, 1.7, 210.),
+            (
+                array![[0., 1., 0.], [0., 2., 0.], [0., 3., 0.]],
+                1.,
+                1.7,
+                3.15,
+            ),
+            (
+                array![[0., -1., 0.], [0., 2., 0.], [0., 3., 0.]],
+                1.,
+                1.7,
+                5.15,
+            ),
+            (
+                array![[0., 1., 0.], [0., 1., 0.], [0., 1., 0.]],
+                1.,
+                1.7,
+                0.,
+            ),
+            (
+                array![[0., 1., 0.], [0., 2., 0.], [0., 3., 0.]],
+                1.,
+                1.3,
+                3.35,
+            ),
+            (
+                array![[0., 1., 0.], [0., 2., 0.], [0., 3., 0.]],
+                1.5,
+                1.7,
+                2.1,
+            ),
+            (
+                array![[0., 10., 0.], [0., 20., 0.], [0., 30., 0.]],
+                1.5,
+                1.7,
+                210.,
+            ),
         ];
         for (u, gamma, delx, expected) in test_cases {
-            assert_eq!(du2dx_kernel(ArrayView1::from(&u), gamma, delx), expected);
+            assert_eq!(du2dx_kernel(ArrayView2::from(&u), gamma, delx), expected);
         }
     }
 }
