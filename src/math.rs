f1c791014e1959287b22784ab663fd14a7cc6518
Mike Stipicevic <stipredirect@gmail.com>
2025-02-27 16:27:10 +0100

    Implement DV2DY

--- src/math.rs
+++ src/math.rs
@@ -119,6 +119,36 @@ pub fn duvdy(
     (left_side + (gamma * (inner_left2 - inner_right2))) / (4.0 * dely)
 }
 
+/// Calculates dv^2/dy (the derivative of v^2 over y)
+///
+/// This function uses the same basic algebra rearrangement that the
+/// NaSt2D code does. This makes the function easier to compare against
+/// NaSt2D (and reduces the number of divisions in the calculation).
+///
+/// # Arguments
+///
+/// * `v_view` - A 3x3-element ArrayView2 representing
+///   v[(i-1) to (i+1), (j-1) to (j+1)]. This function only uses
+///   the three values on the column where i is 0 (index 1), but takes a 3x3
+///   ArrayView2 to be easier to combine with other functions.
+/// * `dely` - "delta y," the physical width of the cell
+/// * `gamma` - Greek letter gamma, the upwind discretization parameter
+pub fn dv2dy(v_view: ArrayView2<Real>, dely: Real, gamma: Real) -> Real {
+    let v_i_j = v_view[(1, 1)]; // v[(i, j)] -> v_i_j
+    let v_i_j_p1 = v_view[(1, 2)]; // v[(i, j+1)] -> "v[i][j plus 1]" -> v_i_j_p1
+    let v_i_j_m1 = v_view[(1, 0)]; // v[(i, j-1)] -> "v[i][j minus 1]" -> v_i_j_m1
+
+    let inner_left1 = (v_i_j + v_i_j_p1).powi(2);
+    let inner_right1 = (v_i_j_m1 + v_i_j).powi(2);
+
+    let left_side = inner_left1 - inner_right1;
+
+    let inner_left2 = (v_i_j + v_i_j_p1).abs() * (v_i_j - v_i_j_p1);
+    let inner_right2 = (v_i_j_m1 + v_i_j).abs() * (v_i_j_m1 - v_i_j);
+
+    (left_side + (gamma * (inner_left2 - inner_right2))) / (4.0 * dely)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -249,6 +279,52 @@ mod tests {
                 duvdy(ArrayView2::from(&u), ArrayView2::from(&v), dely, gamma),
                 expected
             );
+        }
+    }
+
+    #[test]
+    fn test_dv2dy() {
+        // These don't have any particular significance, just some random data.
+        let test_cases = [
+            (
+                array![[0., 0., 0.], [1., 2., 3.], [0., 0., 0.]],
+                1.,
+                1.7,
+                3.15,
+            ),
+            (
+                array![[0., 0., 0.], [-1., 2., 3.], [0., 0., 0.]],
+                1.,
+                1.7,
+                5.15,
+            ),
+            (
+                array![[0., 0., 0.], [1., 1., 1.], [0., 0., 0.]],
+                1.,
+                1.7,
+                0.,
+            ),
+            (
+                array![[0., 0., 0.], [1., 2., 3.], [0., 0., 0.]],
+                1.,
+                1.3,
+                3.35,
+            ),
+            (
+                array![[0., 0., 0.], [1., 2., 3.], [0., 0., 0.]],
+                1.5,
+                1.7,
+                2.1,
+            ),
+            (
+                array![[0., 0., 0.], [10., 20., 30.], [0., 0., 0.]],
+                1.5,
+                1.7,
+                210.,
+            ),
+        ];
+        for (v, dely, gamma, expected) in test_cases {
+            assert_eq!(dv2dy(ArrayView2::from(&v), dely, gamma), expected);
         }
     }
 }
