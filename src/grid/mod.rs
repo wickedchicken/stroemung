694f227470e4fb38c7fe24d67e9811b0fb76e1ea
Mike Stipicevic <stipredirect@gmail.com>
2025-02-28 17:29:12 +0100

    Implement BoundaryList
    Currently only calculates the list of boundaries at initialization.

--- src/grid/mod.rs
+++ src/grid/mod.rs
@@ -1,5 +1,6 @@
 pub mod presets;
 
+use std::collections::BTreeSet;
 use std::error::Error;
 use std::fmt;
 use std::io::Read;
@@ -7,9 +8,31 @@ use std::io::Read;
 use serde::Deserialize;
 use serde::Serialize;
 
+use ndarray::Zip;
+
 use crate::cell::Cell;
 use crate::math::Real;
-use crate::types::{GridArray, GridSize};
+use crate::types::{BoundaryIndex, GridArray, GridIndex, GridSize};
+
+#[derive(Debug, Default)]
+struct BoundaryList {
+    boundaries: BTreeSet<BoundaryIndex>,
+    sorted_boundary_list: Vec<GridIndex>,
+}
+
+impl std::fmt::Display for BoundaryList {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "Boundaries:")?;
+        for elem in &self.boundaries {
+            writeln!(f, "  {:?}", elem)?;
+        }
+        writeln!(f, "Sorted Boundary List:")?;
+        for elem in &self.sorted_boundary_list {
+            writeln!(f, "  {:?}", elem)?;
+        }
+        Ok(())
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct UnfinalizedSimulationGrid {
@@ -20,8 +43,9 @@ pub struct UnfinalizedSimulationGrid {
     cell_type: GridArray<Cell>,
 }
 
-// A wrapper struct to make sure that we never deserialize without forgetting
-// to do some postprocessing.
+// This must be the same as UnfinalizedSimulationGrid, except for boundaries.
+// We have two types to make sure we never deserialize without forgetting to
+// generate the boundary list.
 #[derive(Debug, Serialize)]
 pub struct SimulationGrid {
     size: GridSize,
@@ -29,19 +53,24 @@ pub struct SimulationGrid {
     u: GridArray<Real>,
     v: GridArray<Real>,
     cell_type: GridArray<Cell>,
+    #[serde(skip)]
+    boundaries: BoundaryList,
 }
 
 impl From<UnfinalizedSimulationGrid> for SimulationGrid {
     fn from(item: UnfinalizedSimulationGrid) -> Self {
         // Will be nicer once https://github.com/rust-lang/rust/issues/86555
         // is in stable.
-        SimulationGrid {
+        let mut grid = SimulationGrid {
             size: item.size,
             pressure: item.pressure,
             u: item.u,
             v: item.v,
             cell_type: item.cell_type,
-        }
+            boundaries: Default::default(),
+        };
+        grid.rebuild_boundary_list();
+        grid
     }
 }
 
@@ -57,6 +86,27 @@ impl std::fmt::Display for SimulationGrid {
 }
 
 impl SimulationGrid {
+    fn rebuild_boundary_list(&mut self) {
+        self.boundaries.boundaries.clear();
+        // Run a for_each with the value and indices. See
+        // https://github.com/rust-ndarray/ndarray/issues/1093 for details.
+        Zip::indexed(self.cell_type.view()).for_each(|idx, val| {
+            if let Cell::Boundary(_) = val {
+                self.boundaries
+                    .boundaries
+                    .insert(BoundaryIndex(idx.0, idx.1));
+            }
+        });
+        dbg!(&self.boundaries.boundaries);
+        self.boundaries.sorted_boundary_list = self
+            .boundaries
+            .boundaries
+            .iter()
+            .copied()
+            .map(|x| (x.0, x.1))
+            .collect();
+    }
+
     pub fn from_reader<R: Read>(reader: R) -> Result<SimulationGrid, Box<dyn Error>> {
         let unfinalized: UnfinalizedSimulationGrid = serde_json::from_reader(reader)?;
         Ok(SimulationGrid::from(unfinalized))
@@ -66,6 +116,7 @@ impl SimulationGrid {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use ndarray::Array;
     use std::fs::File;
     use std::io::BufReader;
     use std::path::{Path, PathBuf};
@@ -92,6 +143,47 @@ mod tests {
     }
 
     #[test]
+    fn rebuild_boundary_list() {
+        use crate::cell::{BoundaryCell, Cell};
+        let size = [3, 3];
+        let mut unfinalized = UnfinalizedSimulationGrid {
+            size,
+            pressure: Array::zeros(size),
+            u: Array::zeros(size),
+            v: Array::zeros(size),
+            cell_type: Array::from_elem(size, Cell::Fluid),
+        };
+
+        // Everything except for the middle cell
+        let expected_boundaries: Vec<GridIndex> = vec![
+            (0, 0),
+            (0, 1),
+            (0, 2),
+            (1, 0),
+            (1, 2),
+            (2, 0),
+            (2, 1),
+            (2, 2),
+        ];
+        let expected_boundary_indices: Vec<BoundaryIndex> = expected_boundaries
+            .iter()
+            .map(|x| BoundaryIndex(x.0, x.1))
+            .collect();
+
+        for idx in &expected_boundaries {
+            unfinalized.cell_type[*idx] = Cell::Boundary(BoundaryCell::NoSlip);
+        }
+
+        let grid = SimulationGrid::from(unfinalized);
+
+        let calculated_boundaries_as_list: Vec<BoundaryIndex> =
+            grid.boundaries.boundaries.iter().copied().collect();
+
+        assert_eq!(calculated_boundaries_as_list, expected_boundary_indices);
+        assert_eq!(grid.boundaries.sorted_boundary_list, expected_boundaries);
+    }
+
+    #[test]
     fn deserialize() {
         let test_filename = test_data_directory().join("simple_grid.json");
         let result = SimulationGrid::from_reader(BufReader::new(
@@ -99,6 +191,18 @@ mod tests {
         ))
         .unwrap();
         insta::assert_json_snapshot!(result);
+    }
+
+    #[test]
+    fn deserialize_boundaries() {
+        let test_filename = test_data_directory().join("small_grid_with_boundaries.json");
+        let result = SimulationGrid::from_reader(BufReader::new(
+            File::open(test_filename).unwrap(),
+        ))
+        .unwrap();
+
+        insta::assert_json_snapshot!(result);
+        insta::assert_snapshot!(result.boundaries);
     }
 
     #[test]
--- src/types.rs
+++ src/types.rs
@@ -6,3 +6,16 @@ pub type Velocity = [Real; 2];
 pub type GridArray<T> = Array<T, Ix2>;
 
 pub type GridSize = [usize; 2];
+
+// According to https://docs.rs/ndarray/0.16.1/ndarray/struct.ArrayBase.html,
+// "The default memory order of an array is row major order (a.k.a “c” order),
+// where each row is contiguous in memory." This means we can use the default
+// implementation of PartialOrd and Ord. If we were using column-major ordering,
+// we would need to implement an ordering that prioritized the y value over the
+// x value.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct BoundaryIndex(pub usize, pub usize);
+
+// It would be nice to unify BoundaryIndex and GridIndex into one type that
+// can be sorted and also directly used by ndarray's indexing operations.
+pub type GridIndex = (usize, usize);
